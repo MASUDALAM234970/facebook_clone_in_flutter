6312f45b62e9674594c962db14ad6de5c845630c
mdmas <mdmasudalam2349@gmail.com>
2025-07-13 14:23:01 +0600

    Prepared and Create Basic file

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,17 @@
+//import 'package:facebook_clone/core/screens/profile_screen.dart';
+//import 'package:facebook_clone/features/friends/presentation/screens/friends_screen.dart';
+//import 'package:facebook_clone/features/posts/presentation/screens/posts_screen.dart';
+//import 'package:facebook_clone/features/posts/presentation/screens/videos_screen.dart';
+import 'package:flutter/material.dart';
+
+class Constants {
+  // Default padding for screens
+  static const defaultPadding = EdgeInsets.symmetric(
+    horizontal: 15,
+    vertical: 10,
+  );
+
+
+
+  Constants._();
+}
